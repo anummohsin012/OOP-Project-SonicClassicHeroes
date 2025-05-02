6718e97e0f7af2797961454e6e94a60e82c949ef
anummohsin012 <anummohsinr@gmail.com>
2025-05-03 00:02:27 +0500

    .

--- Source.cpp
+++ Source.cpp
@@ -7,6 +7,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
+#include "player.cpp"
 
 using namespace sf;
 using namespace std;
@@ -22,8 +23,6 @@ void display_level(RenderWindow& window, const int height, const int width, char
 
 int main()
 {	
-	DDSDS;
-	usman;
 
 	RenderWindow window(VideoMode(screen_x, screen_y), "Sonic the Hedgehog-OOP", Style::Close);
 	window.setVerticalSyncEnabled(true);
